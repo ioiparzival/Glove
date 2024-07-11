b8f47dac087650f01c39d885f7c87c949a5023c1
ioiparzival <opshytoshnikita@gmail.com>
2024-07-11 11:55:27 +0300

    added glove.h

---
+++ src/Glove.h
@@ -0,0 +1,35 @@
+#include <WiFi.h>
+
+#define FOR_3 for(int i = 0; i < 3; i++)
+
+const char *ssid = "CAR_WIFI";
+const char *password = "12345678";
+int sens_val[3], PIN_SENS[3] = {33, 32, 35}, PIN_LED[3] = {19, 18, 5};
+
+WiFiClient master;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Start!");
+
+  FOR_3 ledcSetup(i, 40000, 8); // настройка шим для светодиодов
+                                // (i-канал;40000-частота;8(біт)-разрешение)
+  FOR_3 ledcAttachPin(PIN_LED[i], i); // настройка связи пинов с их каналами
+
+  connectToWiFi();
+  connectToServer();
+}
+
+void loop()
+{
+  Enter_and_convert_values(); // Ввод c АЦП значений напряжения и
+                              // преобразование в нужний диапозон для
+                              // последующей подачи значений машинке
+  Values_per_LED(); // Подача значений в виде шим сигнала на светодиод
+  FOR_3 Serial.printf("sens_val[%d] = %d\n", i,
+                      sens_val[i]); // Выводим значения в Serial порт
+  Transmit_string_wifi(); // Передаем строку по wifi машинке
+  Wifi_check();           // Проверяем контакт с wifi
+  delay(50);
+}
